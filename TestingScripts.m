5228032e238202a5ae1cc49a6bd9bbbe6772c6cb
Pavel Rubis <rubisiay@gmail.com>
2020-04-13 18:18:36 +0300

    Предохнарительный коммит перед заменой диска на компьютере.

---
+++ TestingScripts.m
@@ -0,0 +1,169 @@
+clear; clc;
+format long;
+
+fieldType=true;
+N=5;
+coorditates=[];
+cellCount=0;
+if fieldType
+    cellCount=N*(N-1)*3;
+    
+    i=1:N-1;
+    i=i';
+    i_old=i;
+    for c=1:(cellCount/(N-1))-1
+        i=[i;i_old];
+    end
+    
+    j=ones(N*(N-1),1);
+    for c=0:N-1
+        j(1+(4*c):(N-1)*(c+1),:)=c;
+    end
+    j_old=j;
+    j=[j;j_old];
+    j=[j;j_old];
+    
+    
+    k=ones(N*(N-1)*3,1);
+    k(1:(cellCount/3),:)=1;
+    k((cellCount/3)+1:(2*cellCount/3),:)=2;
+    k((2*cellCount/3)+1:cellCount,:)=3;
+    
+    coorditates=[coorditates i j k];
+   
+else
+    for x=0:N-1
+        for y=0:N-1     
+            coorditates=[coorditates; x y];
+        end
+    end
+end
+
+Re=0:0.001:2;
+Im=0:0.001:2;
+Re=Im(randperm(length(Re)));
+Im=Re(randperm(length(Im)));
+compArr=arrayfun(@(re,im) complex(re,im),Re,Im);
+compArr=compArr';
+Im=Im';
+Re=Re';
+Im=Im(1:cellCount,:);
+Re=Re(1:cellCount,:);
+
+% fileID = fopen('source.txt', 'w');
+% fprintf(fileID,'%d %4.4f\n',y);
+% fclose(fileID);
+% 
+% for i=1:length(compArr)
+%     fprintf(fid, '%f\n',compArr(i));
+% end
+% fclose(fid);
+
+% 
+% 
+% miu=1;
+% f = @(z)miu*(exp(i*z));
+% mapz_zero=@(z) abs(f(z)-z);
+% z0=-3.5+0.5*i;
+% mapz_zero_xy=@(z) mapz_zero(z(1)+i*z(2));
+% [zeq,zer]=fminsearch(mapz_zero_xy, [real(z0) imag(z0)],optimset('TolX',1e-9));
+% [zeq zer]
+% 
+% s.cells=[];
+% N=5;
+% 
+% ResultsProcessing.GetSetFieldOrient(0);
+% ResultsProcessing.GetSetCellOrient(2);
+% 
+% figure; 
+% hold on;
+% 
+% for x=0:N-1
+%     for y=0:N-1     
+%         cell=CACell(0,[],[x,y,0],[rand(1) rand(1) rand(1)],ResultsProcessing.GetSetFieldOrient,N);
+%         s.cells=[s.cells cell];
+%     end
+% end
+% 
+% arrayfun(@(cell) ResultsProcessing.DrawCell(cell),s.cells);
+% 
+% isNeib=zeros(1,N*N);
+% 
+% CA_cell=CACell(0,[],[randi(N-1) randi(N-1) 0],[rand(1) rand(1) rand(1)],ResultsProcessing.GetSetFieldOrient,N);
+% 
+% logicNeib = arrayfun(@(cell) isequal(abs(cell.Indexes - CA_cell.Indexes),[0 1 0]) || isequal(abs(cell.Indexes - CA_cell.Indexes),[1 0 0]),s.cells);
+% neibArrNumbs=find(logicNeib);
+% 
+% if ~isempty(neibArrNumbs)
+%     CA_cell.CurrNeighbors=[CA_cell.CurrNeighbors s.cells(neibArrNumbs(:))];
+% end
+% 
+% axis image
+% set(gca,'xtick',[])
+% set(gca,'ytick',[])
+% set(gca,'Visible','off')
+% 
+% 
+% 
+% s.cells=[];
+% N=1;
+% 
+% ResultsProcessing.GetSetFieldOrient(1);
+% ResultsProcessing.GetSetCellOrient(1);
+% 
+% figure; 
+% hold on;
+% 
+% for k=1:3
+%     for j = 0:N-1
+%         for i = 1:N-1
+%             cell=CACell(complex(randn,randn),[],[i,j,k],[1 1 0],ResultsProcessing.GetSetFieldOrient,N);
+%             s.cells=[s.cells cell];
+%         end
+%     end
+% end
+% 
+% values=randn(1,length(s.cells));
+% for i=1:length(s.cells)
+%     s.cells(i).z0=values(i);
+% end
+% 
+% colors=colormap(jet(length(s.cells)));
+% modulesArr=zeros(1,length(s.cells));
+% 
+% modulesArr=arrayfun(@(cell) ResultsProcessing.GetCellValueModule(cell),s.cells);
+% [modulesArr, cellArrIndexes]=sort(modulesArr);
+% for i=1:length(s.cells)
+%     s.cells(cellArrIndexes(i)).Color=colors(i,:);
+% end
+% 
+% arrayfun(@(cell) ResultsProcessing.DrawCell(cell),s.cells);
+% 
+% CA_cell=CACell(0,[],[2 2 1],[0 0 1],ResultsProcessing.GetSetFieldOrient,N);
+% % ResultsProcessing.DrawCell(CA_cell)
+% 
+% logicNeib = arrayfun(@(cell) isequal(abs(cell.Indexes - CA_cell.Indexes),[0 1 0]) || isequal(abs(cell.Indexes - CA_cell.Indexes),[1 0 0]) || isequal(cell.Indexes - CA_cell.Indexes,[1 1 0])  || isequal(cell.Indexes - CA_cell.Indexes,[-1 -1 0]),s.cells);
+% neibArrNumbs=find(logicNeib);
+% if ~isempty(neibArrNumbs)
+%     CA_cell.CurrNeighbors=[CA_cell.CurrNeighbors s.cells(neibArrNumbs(:))];
+% end
+% 
+% 
+% for i=1:length(CA_cell.CurrNeighbors)
+%     CA_cell.CurrNeighbors(i).Color=[1 0 0];
+% end
+% 
+% % 
+% 
+% % arrayfun(@(cell) ResultsProcessing.DrawCell(cell),CA_cell.CurrNeighbors);
+% 
+% axis image
+% set(gca,'xtick',[])
+% set(gca,'ytick',[])
+% set(gca,'Visible','off')
+% 
+% function out = ComplexModule(compNum)
+%     out=sqrt(real(compNum)*real(compNum)+imag(compNum)*imag(compNum));
+% end
+% 
+
